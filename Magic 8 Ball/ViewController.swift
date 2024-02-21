aa9739bf3b008204294234018ba33831b9672613
SenaGurkan <senagurkann@gmail.com>
2024-02-21 03:25:45 +0300

    post editing

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var ballImage: UIImageView!
+    
 
-
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let ballArray = [ #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball1") ]
+        ballImage.image = ballArray.randomElement()
+    }
+    
 }
 
